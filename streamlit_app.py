b5a0504b01590c69aea4873de71dc3c84a136772
Olivia Nevanda Hasibuan <77237351+olivianevandaa@users.noreply.github.com>
2023-06-29 17:57:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit
-
+import pandas
+import requests
+import snowflake.connector
+import psycopg2
+from urllib.error import URLError
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast with blue cheese')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
